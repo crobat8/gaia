27799df818a2dde8289dbdc02cc605c42f31fbf0
crobat8 <133931076+crobat8@users.noreply.github.com>
2023-08-02 02:16:41 -0700

    got it working with buttons

--- src/App.js
+++ src/App.js
@@ -1,23 +1,134 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState,useRef, useEffect  } from "react";
+import "./style.scss"
 
 function App() {
+  const [gridLength,setGridLength] = useState(20);
+  const [gridHeight,setGridHeight] = useState(20);
+  const [cells,setCells] = useState([[0,0,0],[0,0,0],[0,0,0]])
+  const [delay,setDelay] = useState(1000)
+  
+  const windowSize = useRef([window.innerWidth, window.innerHeight]);
+
+  function year(){
+    
+    const temp = [];
+    var border = new Array(gridLength+2).fill(0)
+    temp.push(border)
+    for(var x=1;x<gridHeight+1;x++){
+      var tempRow = new Array();
+      tempRow.push(0)
+      for(var y=1;y<gridLength+1;y++){
+        var count = 0
+        count = cells[x-1][y-1]+cells[x-1][y]+cells[x-1][y+1]+
+                cells[x][y-1]  +0            +cells[x][y+1]  +
+                cells[x+1][y-1]+cells[x+1][y]+cells[x+1][y+1]
+        if(cells[x][y] == 1){
+          if(count==2||count==3){
+            tempRow.push(1)
+          }else{
+            tempRow.push(0)
+          }
+        }else{
+          if(count==3){
+            tempRow.push(1)
+          }else{
+            tempRow.push(0)
+          }
+        }
+        
+      }
+      tempRow.push(0)
+      temp.push(tempRow);
+    }
+    temp.push(border)
+    
+    setCells(temp)
+    
+  }
+  
+  function reset(){
+    const temp = [];
+    var border = new Array(gridLength+2).fill(0)
+    temp.push(border)
+    for(var x=0;x<gridHeight;x++){
+      var tempRow = new Array();
+      tempRow.push(0)
+      for(var y=0;y<gridLength;y++){
+        if(Math.random()<0.5){
+          tempRow.push(0)
+        }else{
+          tempRow.push(1)
+        }
+      }
+      tempRow.push(0)
+      temp.push(tempRow);
+    }
+    temp.push(border)
+    setCells(temp)
+  }
+
+  function flip(x,y){
+    if(cells[x][y]==1){
+      cells[x][y]=0
+      setCells(cells)
+    }else{
+      cells[x][y]=1
+      setCells(cells)
+    }
+    
+  }
+
+  useEffect(()=>{
+    reset();
+  },[])
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="navbar">
+        <button onClick={()=>year()}>
+          next year
+        </button>
       </header>
+      <main className="focus">
+        <div className="table">
+          {cells.map((row,x)=>{
+            var cellHeight= ((windowSize.current[1]-100)/gridHeight)-2
+            var cellWidth = (windowSize.current[0]/gridLength)-2
+            return(
+              <div className="row" key={x} >
+                {row.map((cell,y)=>{
+                  if(x==0||x==gridLength+1||y==0||y==gridHeight+1){
+
+                  }else{
+                    if(!!cell){
+                      //console.log("cell",cell,"x",x,"y",y)
+                      return(
+
+                      <div className="cell" onClick={()=>flip(x,y)} style={{"backgroundColor":"lime","height":cellHeight,"width":cellWidth}}>
+                        
+                      </div>
+                      )
+                    }else{
+                      // console.log("empty")
+                      return(
+                        <div className="empty" onClick={()=>flip(x,y)} style={{"backgroundColor":"#202020","height":cellHeight,"width":cellWidth}}>
+                          
+                        </div>
+                      )
+                    }
+                  }
+                  
+                })} 
+              </div>
+            )
+              
+            
+            
+            
+          })}
+
+        </div>
+      </main>
     </div>
   );
 }
